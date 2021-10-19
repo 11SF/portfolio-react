452608cc3f8bc737ed61a84aec86bd3459bf9d73
11SF <nsfolknsf@gmail.com>
2021-10-19 15:56:26 +0700

    edit data

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,29 +3,19 @@ import { Container } from 'react-bootstrap'
 
 import './Portfolio.css'
 import ProjectCard from './ProjectCard'
+import {projectList} from "../../data/data"
 
-import cs264 from "../../assets/images/project/cs264-project.netlify.app_.png";
-import cs251 from "../../assets/images/project/cs251.png";
-import family_pay from "../../assets/images/project/family-pay-14de8.web.app_spotify_FFmZb (1).png";
-import netflix_clone from "../../assets/images/project/netflix-clone-11sf.netlify.app_.png";
-import popgop from "../../assets/images/project/popgop.netlify.app_.png";
-import spotify_flutter from "../../assets/images/project/spotify_flutter.jpg";
 function Portfolio() {
     return (
         <div className="portfolio_area">
             <Container>
                 <h1 style={{textAlign:"end", fontSize:"58px", color:"#ffffff"}}>Portfolio</h1>
                 <div className="content_area">
-                    <ProjectCard img={cs264} name="Late Regisration System for CS Department"/> 
-                    <ProjectCard img={cs251} name="Academic Management Systems"/>
-
-                    <ProjectCard img={family_pay} name="Family Pay"/> 
-                    <ProjectCard img={netflix_clone} name="Netflix Clone"/>
-                    <ProjectCard img={popgop} name="POPGOP"/> 
-                    <ProjectCard img={spotify_flutter} name="Spotify Pay Application"/>
-                    {/* <ProjectCard img="" name="Covid-19 today Application"/> */}
-
-                    {/* <ProjectCard img={project} name="ระบบลงทะเบียนเรียนล่าช้า"/>                   */}
+                    {
+                        projectList.map((value, index) => (
+                            <ProjectCard key={index} img={value.img} name={value.name}/> 
+                        ))
+                    }
                 </div>
             </Container>
         </div>
---
+++ src/data/data.js
@@ -0,0 +1,124 @@
+import cs264 from "../assets/images/project/cs264-project.netlify.app_.png";
+import cs251 from "../assets/images/project/cs251.png";
+import family_pay from "../assets/images/project/family-pay-14de8.web.app_spotify_FFmZb (1).png";
+import netflix_clone from "../assets/images/project/netflix-clone-11sf.netlify.app_.png";
+import popgop from "../assets/images/project/popgop.netlify.app_.png";
+import spotify_flutter from "../assets/images/project/spotify_flutter.jpg";
+
+import c_logo from "../assets/icons/c-program.svg";
+import cPlus_logo from "../assets/icons/icons8-c++.svg";
+import java_logo from "../assets/icons/icons8-java.svg";
+import python_logo from "../assets/icons/icons8-python.svg";
+import javaScript_logo from "../assets/icons/icons8-javascript.svg";
+import dart_logo from "../assets/icons/icons8-dart.svg";
+
+import react_logo from "../assets/icons/react-js.svg";
+import vue_logo from "../assets/icons/vue-js.svg";
+import express_logo from "../assets/icons/expressjs-ar21.svg";
+import flutter_logo from "../assets/icons/icons8-flutter.svg";
+import tailwind_logo from "../assets/icons/tailwindcss.svg";
+import bootstrap_logo from "../assets/icons/icons8-bootstrap.svg";
+
+import git_logo from "../assets/icons/icons8-git.svg";
+import docker_logo from "../assets/icons/icons8-docker.svg";
+import selenium_logo from "../assets/icons/icons8-selenium.svg";
+import jwt_logo from "../assets/icons/icons8-json-web-token.svg";
+
+let languages = [
+  {
+    icon: c_logo,
+    name: "C",
+  },
+  {
+    icon: cPlus_logo,
+    name: "C++",
+  },
+  {
+    icon: java_logo,
+    name: "Java",
+  },
+  {
+    icon: python_logo,
+    name: "Python",
+  },
+  {
+    icon: javaScript_logo,
+    name: "JavaScript",
+  },
+  {
+    icon: dart_logo,
+    name: "Dart",
+  },
+];
+let frameworks = [
+  {
+    icon: react_logo,
+    name: "ReactJS",
+  },
+  {
+    icon: vue_logo,
+    name: "VueJS",
+  },
+  {
+    icon: express_logo,
+    name: "ExpressJS",
+  },
+  {
+    icon: flutter_logo,
+    name: "Flutter",
+  },
+  {
+    icon: tailwind_logo,
+    name: "TailwindCSS",
+  },
+  {
+    icon: bootstrap_logo,
+    name: "Bootstrap",
+  },
+];
+let tools = [
+  {
+    icon: git_logo,
+    name: "Git",
+  },
+  {
+    icon: docker_logo,
+    name: "Docker",
+  },
+  {
+    icon: selenium_logo,
+    name: "Selenium",
+  },
+  {
+    icon: jwt_logo,
+    name: "JWT",
+  },
+];
+let projectList = [
+    {
+        img: cs264,
+        name: "Late Regisration System for CS Department"
+    },
+    {
+        img: cs251,
+        name: "Academic Management Systems"
+    },
+    {
+        img: family_pay,
+        name: "Family Pay"
+    },
+    {
+        img: netflix_clone,
+        name: "Netflix Clone"
+    },
+    {
+        img: popgop,
+        name: "POPGOP"
+    },
+    {
+        img: spotify_flutter,
+        name: "Spotify Pay Application"
+    }
+]
+
+export {projectList, languages, frameworks, tools}
